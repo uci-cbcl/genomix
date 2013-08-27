ddeb0210aff5bd7bfb252359d5818c333c9a0650
Jake Biesinger <jake.biesinger@gmail.com>
2013-08-27 01:13:27 -0700

    Split driver utilities into their own file

---
+++ genomix/genomix-driver/src/main/java/edu/uci/ics/genomix/driver/DriverUtils.java
@@ -0,0 +1,195 @@
+package edu.uci.ics.genomix.driver;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.TreeMap;
+import java.util.Map.Entry;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.SequenceFile;
+import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.util.ReflectionUtils;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartUtilities;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.xy.XYDataset;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+
+import edu.uci.ics.genomix.config.GenomixJobConf;
+import edu.uci.ics.genomix.type.NodeWritable;
+import edu.uci.ics.genomix.type.VKmerBytesWritable;
+
+public class DriverUtils {
+    
+    enum NCTypes {
+        HYRACKS,
+        PREGELIX
+    }
+
+    private static final Log LOG = LogFactory.getLog(DriverUtils.class);
+    
+    public static void copyLocalToHDFS(JobConf conf, String localDir, String destDir) throws IOException {
+        LOG.info("Copying local directory " + localDir + " to HDFS: " + destDir);
+        GenomixJobConf.tick("copyLocalToHDFS");
+        FileSystem dfs = FileSystem.get(conf);
+        Path dest = new Path(destDir);
+        dfs.delete(dest, true);
+        dfs.mkdirs(dest);
+
+        File srcBase = new File(localDir);
+        if (srcBase.isDirectory())
+            for (File f : srcBase.listFiles())
+                dfs.copyFromLocalFile(new Path(f.toString()), dest);
+        else
+            dfs.copyFromLocalFile(new Path(localDir), dest);
+        
+        LOG.info("Copy took " + GenomixJobConf.tock("copyLocalToHDFS") + "ms");
+    }
+
+    public static void copyBinToLocal(JobConf conf, String hdfsSrcDir, String localDestDir) throws IOException {
+        LOG.info("Copying HDFS directory " + hdfsSrcDir + " to local: " + localDestDir);
+        GenomixJobConf.tick("copyBinToLocal");
+        FileSystem dfs = FileSystem.get(conf);
+        FileUtils.deleteQuietly(new File(localDestDir));
+
+        // save original binary to output/bin
+        dfs.copyToLocalFile(new Path(hdfsSrcDir), new Path(localDestDir + File.separator + "bin"));
+
+        // convert hdfs sequence files to text as output/text
+        BufferedWriter bw = null;
+        SequenceFile.Reader reader = null;
+        Writable key = null;
+        Writable value = null;
+        FileStatus[] files = dfs.globStatus(new Path(hdfsSrcDir + File.separator + "*"));
+        for (FileStatus f : files) {
+            if (f.getLen() != 0 && !f.isDir()) {
+                try {
+                    reader = new SequenceFile.Reader(dfs, f.getPath(), conf);
+                    key = (Writable) ReflectionUtils.newInstance(reader.getKeyClass(), conf);
+                    value = (Writable) ReflectionUtils.newInstance(reader.getValueClass(), conf);
+                    if (bw == null)
+                        bw = new BufferedWriter(new FileWriter(localDestDir + File.separator + "data"));
+                    while (reader.next(key, value)) {
+                        if (key == null || value == null)
+                            break;
+                        bw.write(key.toString() + "\t" + value.toString());
+                        bw.newLine();
+                    }
+                } catch (Exception e) {
+                    System.out.println("Encountered an error copying " + f + " to local:\n" + e);
+                } finally {
+                    if (reader != null)
+                        reader.close();
+                }
+
+            }
+        }
+        if (bw != null)
+            bw.close();
+        LOG.info("Copy took " + GenomixJobConf.tock("copyBinToLocal") + "ms");
+    }
+
+    static void drawStatistics(JobConf conf, String inputStats, String outputChart) throws IOException {
+        LOG.info("Getting coverage statistics...");
+        GenomixJobConf.tick("drawStatistics");
+        FileSystem dfs = FileSystem.get(conf);
+        
+        // stream in the graph, counting elements as you go... this would be better as a hadoop job which aggregated... maybe into counters?
+        SequenceFile.Reader reader = null;
+        VKmerBytesWritable key = null;
+        NodeWritable value = null;
+        TreeMap<Integer, Long> coverageCounts = new TreeMap<Integer, Long>();        
+        FileStatus[] files = dfs.globStatus(new Path(inputStats + File.separator + "*"));
+        for (FileStatus f : files) {
+            if (f.getLen() != 0) {
+                try {
+                    reader = new SequenceFile.Reader(dfs, f.getPath(), conf);
+                    key = (VKmerBytesWritable) ReflectionUtils.newInstance(reader.getKeyClass(), conf);
+                    value = (NodeWritable) ReflectionUtils.newInstance(reader.getValueClass(), conf);
+                    while (reader.next(key, value)) {
+                        if (key == null || value == null)
+                            break;
+                        Integer cov = java.lang.Math.round(value.getAverageCoverage());
+                        Long count = coverageCounts.get(cov);
+                        if (count == null)
+                            coverageCounts.put(cov, new Long(1));
+                        else 
+                            coverageCounts.put(cov, count + 1);
+                    }
+                } catch (Exception e) {
+                    System.out.println("Encountered an error getting stats for " + f + ":\n" + e);
+                } finally {
+                    if (reader != null)
+                        reader.close();
+                }
+            }
+        }
+        
+        XYSeries series = new XYSeries("Kmer Coverage");
+        for (Entry<Integer, Long> pair : coverageCounts.entrySet()) {
+            series.add(pair.getKey().floatValue(), pair.getValue().longValue());
+        }
+        XYDataset xyDataset = new XYSeriesCollection(series);
+        JFreeChart chart = ChartFactory.createXYLineChart("Coverage per kmer in " + new File(inputStats).getName(),
+                "Coverage", "Count", xyDataset, PlotOrientation.VERTICAL, true, true, false);
+
+        // Write the data to the output stream:
+        FileOutputStream chartOut = new FileOutputStream(new File(outputChart));
+        ChartUtilities.writeChartAsPNG(chartOut, chart, 800, 600);
+        chartOut.flush();
+        chartOut.close();
+        LOG.info("Coverage took " + GenomixJobConf.tock("drawStatistics") + "ms");
+    }
+    
+    
+    static void dumpGraph(JobConf conf, String inputGraph, String outputFasta, boolean followingBuild) throws IOException {
+        LOG.info("Dumping graph to fasta...");
+        GenomixJobConf.tick("dumpGraph");
+        FileSystem dfs = FileSystem.get(conf);
+        
+        // stream in the graph, counting elements as you go... this would be better as a hadoop job which aggregated... maybe into counters?
+        SequenceFile.Reader reader = null;
+        VKmerBytesWritable key = null;
+        NodeWritable value = null;
+        BufferedWriter bw = null;
+        FileStatus[] files = dfs.globStatus(new Path(inputGraph + File.separator + "*"));
+        for (FileStatus f : files) {
+            if (f.getLen() != 0) {
+                try {
+                    reader = new SequenceFile.Reader(dfs, f.getPath(), conf);
+                    key = (VKmerBytesWritable) ReflectionUtils.newInstance(reader.getKeyClass(), conf);
+                    value = (NodeWritable) ReflectionUtils.newInstance(reader.getValueClass(), conf);
+                    if (bw == null)
+                        bw = new BufferedWriter(new FileWriter(outputFasta));
+                    while (reader.next(key, value)) {
+                        if (key == null || value == null)
+                            break;
+                        bw.write(">node_" + key.toString() + "\n");
+                        bw.write(followingBuild ? key.toString() : value.getInternalKmer().toString());
+                        bw.newLine();
+                    }
+                } catch (Exception e) {
+                    System.out.println("Encountered an error getting stats for " + f + ":\n" + e);
+                } finally {
+                    if (reader != null)
+                        reader.close();
+                }
+            }
+        }
+        if (bw != null)
+            bw.close();
+        LOG.info("Dump graph to fasta took " + GenomixJobConf.tock("dumpGraph") + "ms");
+    }
+
+}
--- genomix/genomix-driver/src/main/java/edu/uci/ics/genomix/driver/GenomixDriver.java
+++ genomix/genomix-driver/src/main/java/edu/uci/ics/genomix/driver/GenomixDriver.java
@@ -15,46 +15,25 @@
 
 package edu.uci.ics.genomix.driver;
 
-import java.io.BufferedWriter;
 import java.io.DataOutputStream;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.TreeMap;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.SequenceFile;
-import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.mapred.FileInputFormat;
 import org.apache.hadoop.mapred.FileOutputFormat;
-import org.apache.hadoop.mapred.JobConf;
-import org.apache.hadoop.util.ReflectionUtils;
 import org.kohsuke.args4j.CmdLineException;
-import org.jfree.chart.ChartFactory;
-import org.jfree.chart.ChartUtilities;
-import org.jfree.chart.JFreeChart;
-import org.jfree.chart.plot.PlotOrientation;
-import org.jfree.data.xy.XYDataset;
-import org.jfree.data.xy.XYSeries;
-import org.jfree.data.xy.XYSeriesCollection;
 
 import edu.uci.ics.genomix.config.GenomixJobConf;
 import edu.uci.ics.genomix.config.GenomixJobConf.Patterns;
-import edu.uci.ics.genomix.data.Marshal;
+import edu.uci.ics.genomix.driver.DriverUtils.NCTypes;
 import edu.uci.ics.genomix.hyracks.graph.driver.Driver.Plan;
 import edu.uci.ics.genomix.minicluster.GenomixMiniCluster;
 import edu.uci.ics.genomix.pregelix.format.InitialGraphCleanInputFormat;
@@ -69,21 +48,13 @@ import edu.uci.ics.genomix.pregelix.operator.splitrepeat.SplitRepeatVertex;
 import edu.uci.ics.genomix.pregelix.operator.tipremove.TipRemoveVertex;
 import edu.uci.ics.genomix.pregelix.operator.unrolltandemrepeat.UnrollTandemRepeat;
 import edu.uci.ics.genomix.type.KmerBytesWritable;
-import edu.uci.ics.genomix.type.PositionWritable;
-import edu.uci.ics.genomix.type.VKmerBytesWritable;
-import edu.uci.ics.genomix.type.NodeWritable;
 import edu.uci.ics.hyracks.api.exceptions.HyracksException;
 import edu.uci.ics.pregelix.api.job.PregelixJob;
-import edu.uci.ics.pregelix.core.driver.Driver;
 
 /**
  * The main entry point for the Genomix assembler, a hyracks/pregelix/hadoop-based deBruijn assembler.
  */
 public class GenomixDriver {
-    private enum NCTypes {
-        HYRACKS,
-        PREGELIX
-    }
 
     private static final Log LOG = LogFactory.getLog(GenomixDriver.class);
     private static final String HADOOP_CONF = "hadoop.conf.xml";
@@ -98,158 +69,7 @@ public class GenomixDriver {
             this.getClass());
     private NCTypes curNC = null;
 
-    private void copyLocalToHDFS(JobConf conf, String localDir, String destDir) throws IOException {
-        LOG.info("Copying local directory " + localDir + " to HDFS: " + destDir);
-        GenomixJobConf.tick("copyLocalToHDFS");
-        FileSystem dfs = FileSystem.get(conf);
-        Path dest = new Path(destDir);
-        dfs.delete(dest, true);
-        dfs.mkdirs(dest);
-
-        File srcBase = new File(localDir);
-        if (srcBase.isDirectory())
-            for (File f : srcBase.listFiles())
-                dfs.copyFromLocalFile(new Path(f.toString()), dest);
-        else
-            dfs.copyFromLocalFile(new Path(localDir), dest);
-        
-        LOG.info("Copy took " + GenomixJobConf.tock("copyLocalToHDFS") + "ms");
-    }
-
-    private static void copyBinToLocal(GenomixJobConf conf, String hdfsSrcDir, String localDestDir) throws IOException {
-        LOG.info("Copying HDFS directory " + hdfsSrcDir + " to local: " + localDestDir);
-        GenomixJobConf.tick("copyBinToLocal");
-        FileSystem dfs = FileSystem.get(conf);
-        FileUtils.deleteQuietly(new File(localDestDir));
-
-        // save original binary to output/bin
-        dfs.copyToLocalFile(new Path(hdfsSrcDir), new Path(localDestDir + File.separator + "bin"));
-
-        // convert hdfs sequence files to text as output/text
-        BufferedWriter bw = null;
-        SequenceFile.Reader reader = null;
-        Writable key = null;
-        Writable value = null;
-        FileStatus[] files = dfs.globStatus(new Path(hdfsSrcDir + File.separator + "*"));
-        for (FileStatus f : files) {
-            if (f.getLen() != 0 && !f.isDir()) {
-                try {
-                    reader = new SequenceFile.Reader(dfs, f.getPath(), conf);
-                    key = (Writable) ReflectionUtils.newInstance(reader.getKeyClass(), conf);
-                    value = (Writable) ReflectionUtils.newInstance(reader.getValueClass(), conf);
-                    if (bw == null)
-                        bw = new BufferedWriter(new FileWriter(localDestDir + File.separator + "data"));
-                    while (reader.next(key, value)) {
-                        if (key == null || value == null)
-                            break;
-                        bw.write(key.toString() + "\t" + value.toString());
-                        bw.newLine();
-                    }
-                } catch (Exception e) {
-                    System.out.println("Encountered an error copying " + f + " to local:\n" + e);
-                } finally {
-                    if (reader != null)
-                        reader.close();
-                }
-
-            }
-        }
-        if (bw != null)
-            bw.close();
-        LOG.info("Copy took " + GenomixJobConf.tock("copyBinToLocal") + "ms");
-    }
-
-    private static void drawStatistics(GenomixJobConf conf, String inputStats, String outputChart) throws IOException {
-        LOG.info("Getting coverage statistics...");
-        GenomixJobConf.tick("drawStatistics");
-        FileSystem dfs = FileSystem.get(conf);
-        
-        // stream in the graph, counting elements as you go... this would be better as a hadoop job which aggregated... maybe into counters?
-        SequenceFile.Reader reader = null;
-        VKmerBytesWritable key = null;
-        NodeWritable value = null;
-        TreeMap<Integer, Long> coverageCounts = new TreeMap<Integer, Long>();        
-        FileStatus[] files = dfs.globStatus(new Path(inputStats + File.separator + "*"));
-        for (FileStatus f : files) {
-            if (f.getLen() != 0) {
-                try {
-                    reader = new SequenceFile.Reader(dfs, f.getPath(), conf);
-                    key = (VKmerBytesWritable) ReflectionUtils.newInstance(reader.getKeyClass(), conf);
-                    value = (NodeWritable) ReflectionUtils.newInstance(reader.getValueClass(), conf);
-                    while (reader.next(key, value)) {
-                        if (key == null || value == null)
-                            break;
-                        Integer cov = java.lang.Math.round(value.getAverageCoverage());
-                        Long count = coverageCounts.get(cov);
-                        if (count == null)
-                            coverageCounts.put(cov, new Long(1));
-                        else 
-                            coverageCounts.put(cov, count + 1);
-                    }
-                } catch (Exception e) {
-                    System.out.println("Encountered an error getting stats for " + f + ":\n" + e);
-                } finally {
-                    if (reader != null)
-                        reader.close();
-                }
-            }
-        }
-        
-        XYSeries series = new XYSeries("Kmer Coverage");
-        for (Entry<Integer, Long> pair : coverageCounts.entrySet()) {
-            series.add(pair.getKey().floatValue(), pair.getValue().longValue());
-        }
-        XYDataset xyDataset = new XYSeriesCollection(series);
-        JFreeChart chart = ChartFactory.createXYLineChart("Coverage per kmer in " + new File(inputStats).getName(),
-                "Coverage", "Count", xyDataset, PlotOrientation.VERTICAL, true, true, false);
-
-        // Write the data to the output stream:
-        FileOutputStream chartOut = new FileOutputStream(new File(outputChart));
-        ChartUtilities.writeChartAsPNG(chartOut, chart, 800, 600);
-        chartOut.flush();
-        chartOut.close();
-        LOG.info("Coverage took " + GenomixJobConf.tock("drawStatistics") + "ms");
-    }
     
-    
-    private static void dumpGraph(GenomixJobConf conf, String inputGraph, String outputFasta, boolean followingBuild) throws IOException {
-        LOG.info("Dumping graph to fasta...");
-        GenomixJobConf.tick("dumpGraph");
-        FileSystem dfs = FileSystem.get(conf);
-        
-        // stream in the graph, counting elements as you go... this would be better as a hadoop job which aggregated... maybe into counters?
-        SequenceFile.Reader reader = null;
-        VKmerBytesWritable key = null;
-        NodeWritable value = null;
-        BufferedWriter bw = null;
-        FileStatus[] files = dfs.globStatus(new Path(inputGraph + File.separator + "*"));
-        for (FileStatus f : files) {
-            if (f.getLen() != 0) {
-                try {
-                    reader = new SequenceFile.Reader(dfs, f.getPath(), conf);
-                    key = (VKmerBytesWritable) ReflectionUtils.newInstance(reader.getKeyClass(), conf);
-                    value = (NodeWritable) ReflectionUtils.newInstance(reader.getValueClass(), conf);
-                    if (bw == null)
-                        bw = new BufferedWriter(new FileWriter(outputFasta));
-                    while (reader.next(key, value)) {
-                        if (key == null || value == null)
-                            break;
-                        bw.write(">node_" + key.toString() + "\n");
-                        bw.write(followingBuild ? key.toString() : value.getInternalKmer().toString());
-                        bw.newLine();
-                    }
-                } catch (Exception e) {
-                    System.out.println("Encountered an error getting stats for " + f + ":\n" + e);
-                } finally {
-                    if (reader != null)
-                        reader.close();
-                }
-            }
-        }
-        if (bw != null)
-            bw.close();
-        LOG.info("Dump graph to fasta took " + GenomixJobConf.tock("dumpGraph") + "ms");
-    }
 
     private void buildGraphWithHyracks(GenomixJobConf conf) throws NumberFormatException, IOException {
         startNCs(NCTypes.HYRACKS);
@@ -340,6 +160,7 @@ public class GenomixDriver {
         LOG.info("Building the graph took " + GenomixJobConf.tock("buildGraphWithHadoop"));
     }
 
+    @SuppressWarnings("deprecation")
     private void setOutput(GenomixJobConf conf, Patterns step) {
         prevOutput = curOutput;
         curOutput = conf.get(GenomixJobConf.HDFS_WORK_PATH) + File.separator + String.format("%02d-", stepNum) + step;
@@ -369,7 +190,7 @@ public class GenomixDriver {
         if (localInput != null) {
             conf.set(GenomixJobConf.INITIAL_INPUT_DIR, conf.get(GenomixJobConf.HDFS_WORK_PATH) + File.separator
                     + "00-initial-input-from-genomix-driver");
-            copyLocalToHDFS(conf, localInput, conf.get(GenomixJobConf.INITIAL_INPUT_DIR));
+            DriverUtils.copyLocalToHDFS(conf, localInput, conf.get(GenomixJobConf.INITIAL_INPUT_DIR));
         }
         curOutput = conf.get(GenomixJobConf.INITIAL_INPUT_DIR);
 
@@ -429,7 +250,7 @@ public class GenomixDriver {
                     addJob(ScaffoldingVertex.getConfiguredJob(conf));
                     break;
                 case STATS:
-                    drawStatistics(conf, curOutput, "coverage.png");
+                    DriverUtils.drawStatistics(conf, curOutput, "coverage.png");
                     break;
                 case DUMP_FASTA:
                     dump = true;
@@ -460,9 +281,9 @@ public class GenomixDriver {
         }
 
         if (conf.get(GenomixJobConf.LOCAL_OUTPUT_DIR) != null)
-            copyBinToLocal(conf, curOutput, conf.get(GenomixJobConf.LOCAL_OUTPUT_DIR));
+            DriverUtils.copyBinToLocal(conf, curOutput, conf.get(GenomixJobConf.LOCAL_OUTPUT_DIR));
         if (dump)
-            dumpGraph(conf, curOutput, "genome.fasta", followingBuild);
+            DriverUtils.dumpGraph(conf, curOutput, "genome.fasta", followingBuild);
         if (conf.get(GenomixJobConf.FINAL_OUTPUT_DIR) != null)
             FileSystem.get(conf).rename(new Path(curOutput), new Path(GenomixJobConf.FINAL_OUTPUT_DIR));
 
