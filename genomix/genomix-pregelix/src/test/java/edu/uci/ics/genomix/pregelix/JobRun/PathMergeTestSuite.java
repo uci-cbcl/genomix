5cda5229510b12635c5b18f94d05c4e4bedd2397
Anbang Xu <anbangx@gmail.com>
2013-08-28 15:34:19 -0700

    format test case

--- genomix/genomix-pregelix/src/test/java/edu/uci/ics/genomix/pregelix/JobRun/PathMergeTestSuite.java
+++ genomix/genomix-pregelix/src/test/java/edu/uci/ics/genomix/pregelix/JobRun/PathMergeTestSuite.java
@@ -6,19 +6,19 @@ public class PathMergeTestSuite extends BasicGraphCleanTestSuite{
 
     public static Test suite() throws Exception {
         String pattern ="PathMerge";
-        String testSet[] = {"5"};
-//                "2", "3", "4", "5", "6", "7", "8", "9", "head_6", "head_7",
-//                "LeftAdj", "RightAdj",
-//                "FR", "RF", "head_FR", "head_RF", "twohead_FR", "twohead_RF",
-//                "SelfTandemRepeat", "TandemRepeatWithMergeEdge", 
-//                "TandemRepeatWithUnmergeEdge", "ComplexTandemRepeat",
-//                "SimplePath", "ThreeDuplicate",
-//                "SimpleBridgePath", "BridgePathWithTandemRepeat",
-//                "RingPath", "CyclePath",
-//                "SimpleTreePath", "ComplexTreePath",
-//                "Triangle", "Rectangle", 
-//                "synthetic",
-//                "SmallGenome"
+        String testSet[] = {"2", "3", "4", "5", "6", "7", "8", "9", "head_6", "head_7",
+                "LeftAdj", "RightAdj",
+                "FR", "RF", "head_FR", "head_RF", "twohead_FR", "twohead_RF",
+                "SelfTandemRepeat", "TandemRepeatWithMergeEdge", 
+                "TandemRepeatWithUnmergeEdge", "ComplexTandemRepeat",
+                "SimplePath", "ThreeDuplicate",
+                "SimpleBridgePath", "BridgePathWithTandemRepeat",
+                "RingPath", "CyclePath",
+                "SimpleTreePath", "ComplexTreePath",
+                "Triangle", "Rectangle", 
+                "synthetic",
+                "SmallGenome"
+        };
         init(pattern, testSet);
         BasicGraphCleanTestSuite testSuite = new BasicGraphCleanTestSuite();
         return makeTestSuite(testSuite);
