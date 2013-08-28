843db8928f687bf257b9b9d4c0888ff4cf1cc3c4
Jake Biesinger <jake.biesinger@gmail.com>
2013-08-28 13:32:03 -0700

    Don't use infiniband ports (look into in the future)

--- genomix/genomix-driver/src/main/resources/scripts/getip.sh
+++ genomix/genomix-driver/src/main/resources/scripts/getip.sh
@@ -8,15 +8,15 @@ if [ $OS_NAME = $LINUX_OS ];
 then
     #Get IP Address
     #Prefer Infiniband connection
-    IPADDR=`/sbin/ifconfig ib0 2> /dev/null | grep "inet " | awk '{print $2}' | cut -f 2 -d ':'`
-    if [ "$IPADDR" = "" ]
-    then
+#    IPADDR=`/sbin/ifconfig ib0 2> /dev/null | grep "inet " | awk '{print $2}' | cut -f 2 -d ':'`
+#    if [ "$IPADDR" = "" ]
+#    then
         IPADDR=`/sbin/ifconfig eth0 | grep "inet " | awk '{print $2}' | cut -f 2 -d ':'`
         if [ "$IPADDR" = "" ]
         then
             IPADDR=`/sbin/ifconfig lo | grep "inet " | awk '{print $2}' | cut -f 2 -d ':'`
         fi 
-    fi 
+#    fi 
 else
         IPADDR=`/sbin/ifconfig en1 | grep "inet " | awk '{print $2}' | cut -f 2 -d ':'`
 	if [ "$IPADDR" = "" ]
