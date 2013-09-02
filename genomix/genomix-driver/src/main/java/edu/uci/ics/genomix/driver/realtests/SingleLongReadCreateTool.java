495440b87a1ee94b17abb00a8e3fbc0a811cda5c
Nan Zhang <zhangnan2920214@gmail.com>
2013-09-02 10:23:20 -0700

    add frame size pressure test package in driver

---
+++ genomix/genomix-driver/src/main/java/edu/uci/ics/genomix/driver/realtests/SingleLongReadCreateTool.java
@@ -0,0 +1,102 @@
+package edu.uci.ics.genomix.driver.realtests;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Random;
+
+import org.apache.commons.io.FileUtils;
+
+import edu.uci.ics.genomix.type.KmerBytesWritable;
+
+public class SingleLongReadCreateTool {
+    /**
+     * it is better to set the kmerSize a big value, 
+     * the target path which contain this string will be generated automatically
+     * target path: relative path: longreadfortest
+     */
+    private static final char[] symbols = new char[4];
+
+    static {
+        symbols[0] = 'A';
+        symbols[1] = 'C';
+        symbols[2] = 'G';
+        symbols[3] = 'T';
+    }
+    
+    public enum KmerDir {
+        FORWARD,
+        REVERSE,
+    }
+
+    private final Random random = new Random();
+    private char[] buf;
+    private HashSet<KmerBytesWritable> nonDupSet;
+    private int k;
+    private KmerBytesWritable tempKmer;
+    private KmerBytesWritable tempReverseKmer;
+    private KmerDir curKmerDir = KmerDir.FORWARD;
+    private String targetPath;
+    
+    public SingleLongReadCreateTool(int kmerSize, int length) {
+        if (length < 1)
+            throw new IllegalArgumentException("length < 1: " + length);
+        buf = new char[length];
+        this.k = kmerSize;
+        this.nonDupSet = new HashSet<KmerBytesWritable>(length);
+        KmerBytesWritable.setGlobalKmerLength(kmerSize);
+        tempKmer = new KmerBytesWritable();
+        tempReverseKmer = new KmerBytesWritable();
+        targetPath = "longreadfortest" + File.separator + "singlelongread_1.fastq";
+    }
+
+    public void generateString() {
+        String tmp = "";
+        int count = 4;
+        for (int idx = 0; idx < buf.length;) {
+            buf[idx] = symbols[random.nextInt(4)];
+            if (idx >= k - 1) {
+                tmp = new String(buf, idx - k + 1, k);
+                tempKmer.setByRead(tmp.getBytes(), 0);
+                tempReverseKmer.setByReadReverse(tmp.getBytes(), 0);
+                curKmerDir = tempKmer.compareTo(tempReverseKmer) <= 0 ? KmerDir.FORWARD : KmerDir.REVERSE;
+                switch (curKmerDir.toString()){
+                    case "FORWARD":
+                        if (!nonDupSet.contains(tempKmer)) {
+                            nonDupSet.add(new KmerBytesWritable(tempKmer));
+                            idx++;
+                            count = 4;
+                        } else if (count == 0) {
+                            idx++;
+                            count = 4;
+                        } else
+                            count--;
+                        break;
+                    case "REVERSE":
+                        if (!nonDupSet.contains(tempReverseKmer)) {
+                            nonDupSet.add(new KmerBytesWritable(tempReverseKmer));
+                            idx++;
+                            count = 4;
+                        } else if (count == 0) {
+                            idx++;
+                            count = 4;
+                        } else
+                            count--;
+                        break;
+                }
+            } else
+                idx++;
+        }
+    }
+    
+    public void writeToDisk() throws IOException {
+        String targetStr = new String(buf);
+        FileUtils.writeStringToFile(new File(targetPath), targetStr);
+    }
+    
+    public void cleanDiskFile() throws IOException {
+        File targetFile = new File(targetPath);
+        if(targetFile.exists())
+            targetFile.delete();
+    }
+}
