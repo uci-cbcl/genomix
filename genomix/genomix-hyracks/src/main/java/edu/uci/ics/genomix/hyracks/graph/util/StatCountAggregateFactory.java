11cfa9fc73889d3322cb31934f49172517b4ceb9
Nan Zhang <zhangnan2920214@gmail.com>
2013-09-03 22:54:34 -0700

    change the stat package name in graph building of hyracks

--- genomix/genomix-hyracks/src/main/java/edu/uci/ics/genomix/hyracks/graph/util/StatCountAggregateFactory.java
+++ genomix/genomix-hyracks/src/main/java/edu/uci/ics/genomix/hyracks/graph/util/StatCountAggregateFactory.java
@@ -12,7 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package edu.uci.ics.genomix.hyracks.util;
+package edu.uci.ics.genomix.hyracks.graph.util;
 
 import java.io.DataOutput;
 import java.io.IOException;
