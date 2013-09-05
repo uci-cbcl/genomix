addf565a2400c46632177446c875acbf009b741f
Nan Zhang <zhangnan2920214@gmail.com>
2013-09-05 09:22:03 -0700

    FramSize Test normal run

--- genomix/genomix-driver/src/test/java/edu/uci/ics/genomix/driver/realtests/FrameSizePressureTest.java
+++ genomix/genomix-driver/src/test/java/edu/uci/ics/genomix/driver/realtests/FrameSizePressureTest.java
@@ -12,9 +12,6 @@ public class FrameSizePressureTest {
 
         @Option(name = "-readLength", usage = "the length of this single long read", required = true)
         public int readLength;
-
-        @Option(name = "-coresPerMachine", usage = "how many partitions per machine for hyracks job", required = false)
-        public int coresPerMachine;
     }
 
     public static void main(String[] args) throws Exception {
@@ -28,10 +25,8 @@ public class FrameSizePressureTest {
         System.out.println(System.getProperty("java.class.path"));
         System.out.println(System.getenv("HADOOP_HOME"));
         System.out.println(System.getenv("JAVA_HOME"));
-        String[] fsPressureArgs = { "-kmerLength", String.valueOf(options.kmerLength), 
-//                "-coresPerMachine", String.valueOf(options.coresPerMachine), 
-                "-saveIntermediateResults", "true", "-localInput",
-                test.getTestDir(), "-pipelineOrder", "BUILD_HYRACKS,MERGE" };
+        String[] fsPressureArgs = { "-kmerLength", String.valueOf(options.kmerLength), "-saveIntermediateResults",
+                "true", "-localInput", test.getTestDir(), "-pipelineOrder", "BUILD_HYRACKS,MERGE" };
         GenomixDriver.main(fsPressureArgs);
     }
 
