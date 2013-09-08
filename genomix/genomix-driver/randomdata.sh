e7afab628fe0c1821c9902610a4f66db93909ce4
Anbang Xu <anbangx@gmail.com>
2013-09-08 16:55:30 -0700

    add more details in randomData.sh

--- genomix/genomix-driver/randomdata.sh
+++ genomix/genomix-driver/randomdata.sh
@@ -2,11 +2,11 @@
 
 #set -o pipefail
 #set -e
-set -x
+#set -x
 
 if [ $# -ne 5 ]; then
     echo "please provide 5 parameters: infile.readids numlines numfiles outdir and cmd" 
-    echo "for example:   $0 /data/users/anbangx/testdata/5k_assemblathon_readids/5k_assemblathon.readids 100 5 ~/subset  \"bin/genomix -kmerLength 55 -localOutput ~/result/500k_reads_P4 -pipelineOrder BUILD_HADOOP,MERGE -localInput \""
+    echo "for example:   $0 /data/users/anbangx/testdata/5k_assemblathon_readids/5k_assemblathon.readids 100 5 ~/subset  \"/data/users/anbangx/hyracks/genomix/genomix-driver/target/appassembler/bin/genomix -kmerLength 55 -localOutput ~/result/500k_reads_P4 -pipelineOrder BUILD_HADOOP,MERGE -localInput \""
     exit 1
 fi
 
@@ -17,6 +17,8 @@ OUTDIR=$4
 CMD=$5
 
 rm -rf $OUTDIR
+rm -rf $OUTDIR/logs
+mkdir -p $OUTDIR/logs
 
 for i in `seq 1 $NUM_FILES`;
 do
