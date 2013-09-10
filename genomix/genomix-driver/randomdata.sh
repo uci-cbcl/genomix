3681903aa623b8a3b9ca44d3990fbfa94af721c9
Anbang Xu <anbangx@gmail.com>
2013-09-10 11:55:44 -0700

    Change sort to shuf

--- genomix/genomix-driver/randomdata.sh
+++ genomix/genomix-driver/randomdata.sh
@@ -6,7 +6,7 @@
 
 if [ $# -ne 5 ]; then
     echo "please provide 5 parameters: infile.readids numlines numfiles outdir and cmd" 
-    echo "for example:   $0 /data/users/anbangx/testdata/5k_assemblathon_readids/5k_assemblathon.readids 100 5 ~/subset  \"/data/users/anbangx/hyracks/genomix/genomix-driver/target/appassembler/bin/genomix -kmerLength 55 -localOutput ~/result/500k_reads_P4 -pipelineOrder BUILD_HADOOP,MERGE -localInput \""
+    echo "for example:   $0 /ffs/test/cbcl/anbangx/data/ 100 5 /ffs/test/cbcl/anbangx/randomset  \"/data/users/anbangx/hyracks/genomix/genomix-driver/target/appassembler/bin/genomix -kmerLength 55 -pipelineOrder BUILD_HYRACKS,MERGE \""
     exit 1
 fi
 
@@ -22,9 +22,11 @@ mkdir -p $OUTDIR/logs/error
 for i in `seq 1 $NUM_FILES`;
 do
     mkdir -p $OUTDIR/random_set_$i || (echo "chouldn't make the output dir $OUTDIR/random_set_$i" && exit 1)
-    sort -R $INFILE | head -n $NUMLINES > $OUTDIR/random_set_$i/random.readid
-    eval "$CMD $OUTDIR/random_set_$i/random.readid" 2>&1 | tee $OUTDIR/logs/random_set_$i
-    if [ ${PIPESTATUS[0]} -eq 0 ]; then
+    #sort -R $INFILE | head -n $NUMLINES > $OUTDIR/random_set_$i/random.readid
+    shuf -n $NUMLINES $INFILE > $OUTDIR/random_set_$i/random.readid
+    echo "$CMD -localInput $OUTDIR/random_set_$i/random.readid -localOutput ~/result/randomset -hdfsWorkPath /user/anbangx/result/random_set_$i 2>&1 | tee $OUTDIR/logs/random_set_$i"
+    eval "$CMD -localInput $OUTDIR/random_set_$i/random.readid -localOutput ~/result/randomset -hdfsWorkPath /user/anbangx/result/random_set_$i" 2>&1 | tee $OUTDIR/logs/random_set_$i
+    if [ ${PIPESTATUS[0]} -ne 0 ]; then
         mv $OUTDIR/logs/random_set_$i $OUTDIR/logs/error/random_set_$i
     fi
 done
