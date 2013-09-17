fde129e23c35053ea8aa781801cacb7ea8a8d87b
Jake Biesinger <jake.biesinger@gmail.com>
2013-09-17 11:04:58 -0700

    update kmerNeighbors script

--- genomix/genomix-hadoop/src/test/python/getKmerNeighbors.py
+++ genomix/genomix-hadoop/src/test/python/getKmerNeighbors.py
@@ -2,11 +2,11 @@ __author__ = 'wbiesing'
 
 import sys
 import string
+import pickle
 
 
 
-
-def getneighbors(readids, kmerlength, reads):
+def getneighbors(readids, kmerlength, reads, index, skipreads):
     "return a set of readids containing any of the kmers in the given readid"
     neighbors = set()
     for origreadid in readids:
@@ -14,8 +14,8 @@ def getneighbors(readids, kmerlength, reads):
         seq = reads[origreadid]
         for i in range(len(seq) - kmerlength):
             subseq = seq[i:i+kmerlength]
-            revsubseq = revcomp(subseq)
-            neighbors.update(readid for readid, otherseq in reads.iteritems() if subseq in otherseq or revsubseq in otherseq)
+            neighbors.update(index.get(subseq, set()) - skipreads)
+            neighbors.update(index.get(revcomp(subseq), set()) - skipreads)
     return neighbors
 
 
@@ -25,28 +25,48 @@ def revcomp(s):
 
 
 def main(argv):
-    origkmer = argv[1]
-    infile = argv[2]
-    numhops = int(argv[3])
-    outname = argv[4]
+    try:
+        origkmer = argv[1]
+        infile = argv[2]
+        numhops = int(argv[3])
+        outname = argv[4]
+    except:
+        print "usage: <prog> origkmer infile numhops outname"
+        return -1
 
     kmerlength = len(origkmer)
 
-    print 'reading in', infile
-    reads = {line.strip().split('\t')[0] : line.strip().split('\t')[1] for line in open(infile)}
+    try:
+        print 'reading in', infile + ".reads.pkl"
+        reads = pickle.load(open(infile + ".reads.pkl"))
+    except:
+        print 'reading in', infile
+        reads = {line.strip().split('\t')[0] : line.strip().split('\t')[1] for line in open(infile)}
+        pickle.dump(reads, open(infile + ".reads.pkl", 'w'), -1)
+
+    #try:
+    #    print 'reading', infile + '.index.pkl'
+    #    index = pickle.load(open(infile + ".index.pkl"))
+    #except:
+    index = {}
+    print 'building index'
+    for readid, line in reads.items():
+        for i in range(len(line) - kmerlength):
+            index.setdefault(line[i:i+kmerlength], set()).add(readid)
+    #pickle.dump(index, open(infile + ".index.pkl", 'w'), -1)
+
     outreads = set()
 
     print 'getting matching readids'
     curkmer = origkmer
-    for readid, seq in reads.iteritems():
-        if curkmer in seq or revcomp(curkmer) in seq:
-            outreads.add(readid)
+    outreads.update(index.get(curkmer, set()))
+    outreads.update(index.get(revcomp(curkmer), set()))
 
     # get neighbors
     new_neighbors = outreads
     for i in range(numhops):
         print 'hop', i
-        new_neighbors = getneighbors(new_neighbors, kmerlength, reads)
+        new_neighbors = getneighbors(new_neighbors, kmerlength, reads, index, outreads)
         outreads.update(new_neighbors)
 
     print 'found', len(outreads), 'neighbors'
