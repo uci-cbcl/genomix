3e02877ca37f3036ecde4873c8d9940c516b5d25
Jake Biesinger <jake.biesinger@gmail.com>
2013-09-24 08:03:19 -0700

    refactor names in getKmerNeighbors.py

--- genomix/genomix-hadoop/src/test/python/getKmerNeighbors.py
+++ genomix/genomix-hadoop/src/test/python/getKmerNeighbors.py
@@ -3,24 +3,31 @@ __author__ = 'wbiesing'
 import sys
 import string
 import pickle
+from itertools import imap
 
 
-
-def getneighbors(readids, kmerlength, reads, index, skipreads):
-    "return a set of readids containing any of the kmers in the given readid"
+def getneighbors(ids_to_search, kmerlength, readindex, kmerindex, skipids):
+    """return a set of ids_to_search containing any of the kmers in the given readid
+    :param dict ids_to_search:
+    :param int kmerlength:
+    :param dict readindex:
+    :param dict kmerindex:
+    :param set skipids:
+    """
     neighbors = set()
-    for origreadid in readids:
-        print 'searching ', origreadid
-        seq = reads[origreadid]
-        for i in range(len(seq) - kmerlength):
-            subseq = seq[i:i+kmerlength]
-            neighbors.update(index.get(subseq, set()) - skipreads)
-            neighbors.update(index.get(revcomp(subseq), set()) - skipreads)
+    for rid in ids_to_search:
+        print 'searching ', rid
+        read = readindex[rid]
+        for i in range(len(read) - kmerlength):
+            kmer = read[i:i + kmerlength]
+            neighbors.update(kmerindex.get(kmer, set()) - skipids)
+            neighbors.update(kmerindex.get(revcomp(kmer), set()) - skipids)
     return neighbors
 
 
-_revcomp_table = string.maketrans('ACGT', 'TGCA')
-def revcomp(s):
+def revcomp(s, _revcomp_table=string.maketrans('ACGT', 'TGCA')):
+    """return the reverse-complement of s
+    """
     return string.translate(s, _revcomp_table)[::-1]
 
 
@@ -30,51 +37,40 @@ def main(argv):
         infile = argv[2]
         numhops = int(argv[3])
         outname = argv[4]
-    except:
+    except IndexError:
         print "usage: <prog> origkmer infile numhops outname"
         return -1
 
     kmerlength = len(origkmer)
 
-    try:
-        print 'reading in', infile + ".reads.pkl"
-        reads = pickle.load(open(infile + ".reads.pkl"))
-    except:
-        print 'reading in', infile
-        reads = {line.strip().split('\t')[0] : line.strip().split('\t')[1] for line in open(infile)}
-        pickle.dump(reads, open(infile + ".reads.pkl", 'w'), -1)
+    print 'reading in', infile
+    readindex = {rid: read for rid, read in imap(lambda line: line.strip().split('\t'), open(infile))}
 
-    #try:
-    #    print 'reading', infile + '.index.pkl'
-    #    index = pickle.load(open(infile + ".index.pkl"))
-    #except:
-    index = {}
+    kmerindex = {}
     print 'building index'
-    for readid, line in reads.items():
-        for i in range(len(line) - kmerlength):
-            index.setdefault(line[i:i+kmerlength], set()).add(readid)
-    #pickle.dump(index, open(infile + ".index.pkl", 'w'), -1)
+    for rid, read in readindex.items():
+        for i in range(len(read) - kmerlength):
+            kmerindex.setdefault(read[i:i + kmerlength], set()).add(rid)
 
     outreads = set()
 
     print 'getting matching readids'
     curkmer = origkmer
-    outreads.update(index.get(curkmer, set()))
-    outreads.update(index.get(revcomp(curkmer), set()))
+    outreads.update(kmerindex.get(curkmer, set()))
+    outreads.update(kmerindex.get(revcomp(curkmer), set()))
 
     # get neighbors
     new_neighbors = outreads
     for i in range(numhops):
         print 'hop', i
-        new_neighbors = getneighbors(new_neighbors, kmerlength, reads, index, outreads)
+        new_neighbors = getneighbors(new_neighbors, kmerlength, readindex, kmerindex, outreads)
         outreads.update(new_neighbors)
 
     print 'found', len(outreads), 'neighbors'
 
     with open(outname, 'w') as outfile:
-        for readid in outreads:
-            outfile.write(readid + '\t' + reads[readid] + '\n')
-
+        for rid in outreads:
+            outfile.write(rid + '\t' + readindex[rid] + '\n')
 
 
 if __name__ == '__main__':
