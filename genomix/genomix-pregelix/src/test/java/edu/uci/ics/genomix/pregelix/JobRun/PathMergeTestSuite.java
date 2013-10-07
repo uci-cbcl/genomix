bd86fe59edba3f9b2f67cc4b8b00c4a6902c9b41
Anbang Xu <anbangx@gmail.com>
2013-10-06 22:39:23 -0700

    Reorganize TestSet for pathmerge

--- genomix/genomix-pregelix/src/test/java/edu/uci/ics/genomix/pregelix/JobRun/PathMergeTestSuite.java
+++ genomix/genomix-pregelix/src/test/java/edu/uci/ics/genomix/pregelix/JobRun/PathMergeTestSuite.java
@@ -6,26 +6,20 @@ public class PathMergeTestSuite extends BasicGraphCleanTestSuite{
 
     public static Test suite() throws Exception {
         String pattern ="PathMerge"; 
-        String testSet[] = {"SimpleTreePath", "9", "RingPath", "ThreeNodesCycle", "CyclePath", "P2_8", "ComplexTandemRepeat", "SimpleTreePath"
-//                "2", "3", "4", "5", "6", "7", "8",
-//                "9", 
-//                "head_6", "head_7",
-//                "P2_3", "P2_4", "P2_5", "P2_6", "P2_7", "P2_8",
+        String testSet[] = {"2" //"SimpleTreePath", "9", "RingPath", "ThreeNodesCycle", "CyclePath", "Head_8", "ComplexTandemRepeat", "SimpleTreePath"
+//                "2", "3", "4", "5", "6", "7", "8", "9", 
+//                "Head_5", "Head_6", "Head_7", "Head_8", "Head_9", "Head_10",
 //                "LeftAdj", "RightAdj",
-//                "FR", "RF", "head_FR", "head_RF", "twohead_FR", "twohead_RF",
+//                "SimplePath", "ThreeDuplicate",
+//                "SimpleBridgePath,"SimpleTreePath", "ComplexTreePath",
+//                "RingPath", "CyclePath",
 //                "SelfTandemRepeat", "TandemRepeatWithMergeEdge", 
 //                "TandemRepeatWithUnmergeEdge", "ComplexTandemRepeat",
-//                "SimplePath", "ThreeDuplicate",
-//                "SimpleBridgePath", "BridgePathWithTandemRepeat",
-//                "RingPath", "CyclePath",
-//                "SimpleTreePath", "ComplexTreePath",
-//                "Triangle", "Rectangle", 
-//                "synthetic",
 //                "MultiTandemRepeat", "MultiTandemRepeat2", "MultiTandemRepeat3",
-//                "TandemRepeatWithSmallCycle", "TandemRepeatAndCycle",
-//                "sameWithEdge", "FR_RF_Simple"
-//                "SmallGenome_5",
-
+//                "TandemRepeatWithThreeNodes", "TandemRepeatAndCycle",
+//        		  "ThreeNodesCycle"
+//                "SameGeneCodeWithEdge", "SameGeneCodeWithoutEdge"
+//                "synthetic",
         };
         init(pattern, testSet);
         BasicGraphCleanTestSuite testSuite = new BasicGraphCleanTestSuite();
