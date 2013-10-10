41238e19b16a842aac4519aca40ffb01a977d49f
Anbang Xu <anbangx@gmail.com>
2013-10-09 20:09:27 -0700

    If using Enum, change byte/boolean to it. Temporarily P2 code for clean code

--- genomix/genomix-pregelix/src/main/java/edu/uci/ics/genomix/pregelix/format/P2VertexToNodeInputFormat.java
+++
@@ -1,8 +0,0 @@
-package edu.uci.ics.genomix.pregelix.format;
-
-import edu.uci.ics.genomix.pregelix.io.P2VertexValue;
-
-public class P2VertexToNodeInputFormat 
-	extends GenericVertexToNodeOutputFormat<P2VertexValue>{
-
-}
