19cbf3cb9c906fdcdc40017caf47cd7224e7d10b
Anbang Xu <anbangx@gmail.com>
2013-10-19 18:15:53 -0700

    Cleanup BubbleAdd testsuite

--- genomix/genomix-pregelix/src/test/java/edu/uci/ics/genomix/pregelix/Test/BubbleAddVertex.java
+++ genomix/genomix-pregelix/src/test/java/edu/uci/ics/genomix/pregelix/Test/BubbleAddVertex.java
@@ -6,6 +6,12 @@ import java.util.Iterator;
 
 import org.apache.hadoop.io.NullWritable;
 
+import edu.uci.ics.genomix.config.GenomixJobConf;
+import edu.uci.ics.genomix.pregelix.client.Client;
+import edu.uci.ics.genomix.pregelix.format.NodeToVertexInputFormat;
+import edu.uci.ics.genomix.pregelix.format.VertexToNodeOutputFormat;
+import edu.uci.ics.genomix.pregelix.io.VertexValueWritable;
+import edu.uci.ics.genomix.pregelix.io.message.MessageWritable;
 import edu.uci.ics.genomix.type.EDGETYPE;
 import edu.uci.ics.genomix.type.EdgeMap;
 import edu.uci.ics.genomix.type.ReadIdSet;
@@ -13,12 +19,6 @@ import edu.uci.ics.genomix.type.VKmer;
 import edu.uci.ics.pregelix.api.graph.Vertex;
 import edu.uci.ics.pregelix.api.job.PregelixJob;
 import edu.uci.ics.pregelix.api.util.BspUtils;
-import edu.uci.ics.genomix.config.GenomixJobConf;
-import edu.uci.ics.genomix.pregelix.client.Client;
-import edu.uci.ics.genomix.pregelix.format.NodeToVertexInputFormat;
-import edu.uci.ics.genomix.pregelix.format.VertexToNodeOutputFormat;
-import edu.uci.ics.genomix.pregelix.io.VertexValueWritable;
-import edu.uci.ics.genomix.pregelix.io.message.MessageWritable;
 
 /**
  * Testing tool: Add Bubble
@@ -28,16 +28,25 @@ import edu.uci.ics.genomix.pregelix.io.message.MessageWritable;
  */
 public class BubbleAddVertex extends Vertex<VKmer, VertexValueWritable, NullWritable, MessageWritable> {
     public static int kmerSize = -1;
+    public static final String MAJOR_VERTEXID = "BubbleAddVertex.majorVertexId";
+    public static final String MIDDLE_VERTEXID = "BubbleAddVertex.middleVertexId";
+    public static final String MINOR_VERTEXID = "BubbleAddVertex.minorVertexId";
+    public static final String INSERTED_BUBBLE = "BubbleAddVertex.insertedBubble";
+    public static final String INTERNAL_KMER_IN_NEWBUBBLE = "BubbleAddVertex.internalKmerInNewBubble";
+    public static final String COVERAGE_OF_INSERTED_BUBBLE = "BubbleAddVertex.coverageOfInsertedBubble";
+    public static final String READID = "BubbleAddVertex.readId";
+    public static final String NEWBUBBLE_TO_MAJOR_EDGETYPE = "BubbleAddVertex.newBubbleToMajorEdgetype";
+    public static final String NEWBUBBLE_TO_MINOR_EDGETYPE = "BubbleAddVertex.newBubbleToMinorEdgeType";
 
-    private VKmer majorVertexId = new VKmer("AAT"); //forward
-    private VKmer middleVertexId = new VKmer("ATA"); //forward
-    private VKmer minorVertexId = new VKmer("CTA"); //forward
-    private VKmer insertedBubble = new VKmer("ACA"); //forward
-    private VKmer internalKmerInNewBubble = new VKmer("TTC");
-    private float coverageOfInsertedBubble = 4;
-    private long readId = 2;
-    private EDGETYPE newBubbleToMajorEdgetype = EDGETYPE.RR;
-    private EDGETYPE newBubbleToMinorEdgeType = EDGETYPE.FR;
+    private VKmer majorVertexId = null;
+    private VKmer middleVertexId = null;
+    private VKmer minorVertexId = null;
+    private VKmer insertedBubble = null;
+    private VKmer internalKmerInNewBubble = null;
+    private float coverageOfInsertedBubble = -1;
+    private long readId = -1;
+    private EDGETYPE newBubbleToMajorEdgetype = null;
+    private EDGETYPE newBubbleToMinorEdgeType = null;
 
     private EdgeMap[] edges = new EdgeMap[4];
 
@@ -48,6 +57,36 @@ public class BubbleAddVertex extends Vertex<VKmer, VertexValueWritable, NullWrit
         if (kmerSize == -1)
             kmerSize = Integer.parseInt(getContext().getConfiguration().get(GenomixJobConf.KMER_LENGTH));
         GenomixJobConf.setGlobalStaticConstants(getContext().getConfiguration());
+        if (majorVertexId == null) {
+            majorVertexId = new VKmer(getContext().getConfiguration().get(MAJOR_VERTEXID));
+        }
+        if (middleVertexId == null) {
+            middleVertexId = new VKmer(getContext().getConfiguration().get(MIDDLE_VERTEXID));
+        }
+        if (minorVertexId == null) {
+            minorVertexId = new VKmer(getContext().getConfiguration().get(MINOR_VERTEXID));
+        }
+        if (insertedBubble == null) {
+            insertedBubble = new VKmer(getContext().getConfiguration().get(INSERTED_BUBBLE));
+        }
+        if (internalKmerInNewBubble == null) {
+            internalKmerInNewBubble = new VKmer(getContext().getConfiguration().get(INTERNAL_KMER_IN_NEWBUBBLE));
+        }
+        if (coverageOfInsertedBubble < 0) {
+            coverageOfInsertedBubble = Float.parseFloat(getContext().getConfiguration()
+                    .get(COVERAGE_OF_INSERTED_BUBBLE));
+        }
+        if (readId < 0) {
+            readId = Long.parseLong(getContext().getConfiguration().get(READID));
+        }
+        if (newBubbleToMajorEdgetype == null) {
+            newBubbleToMajorEdgetype = EDGETYPE.fromByte(Byte.parseByte(getContext().getConfiguration().get(
+                    NEWBUBBLE_TO_MAJOR_EDGETYPE)));
+        }
+        if (newBubbleToMinorEdgeType == null) {
+            newBubbleToMinorEdgeType = EDGETYPE.fromByte(Byte.parseByte(getContext().getConfiguration().get(
+                    NEWBUBBLE_TO_MINOR_EDGETYPE)));
+        }
     }
 
     /**
@@ -80,7 +119,8 @@ public class BubbleAddVertex extends Vertex<VKmer, VertexValueWritable, NullWrit
 
     public void addEdgeToInsertedBubble(EDGETYPE meToNewBubbleDir, VKmer insertedBubble) {
         EDGETYPE newBubbleToMeDir = meToNewBubbleDir.mirror();
-        getVertexValue().getEdgeMap(newBubbleToMeDir).put(insertedBubble, new ReadIdSet(Arrays.asList(new Long(readId))));
+        getVertexValue().getEdgeMap(newBubbleToMeDir).put(insertedBubble,
+                new ReadIdSet(Arrays.asList(new Long(readId))));
     }
 
     public void setupEdgeForInsertedBubble() {
