c5c39aa0fd71e0a64801f1d304cb0a0df7bc490f
Anbang Xu <anbangx@gmail.com>
2013-10-19 17:51:43 -0700

    Remove redundant testcase for bubblemerge

--- genomix/genomix-pregelix/src/test/java/edu/uci/ics/genomix/pregelix/JobRun/main/BubbleMergeTestSuite.java
+++ genomix/genomix-pregelix/src/test/java/edu/uci/ics/genomix/pregelix/JobRun/main/BubbleMergeTestSuite.java
@@ -1,17 +1,13 @@
 package edu.uci.ics.genomix.pregelix.JobRun.main;
 
-import edu.uci.ics.genomix.pregelix.JobRun.BasicGraphCleanTestSuite;
 import junit.framework.Test;
+import edu.uci.ics.genomix.pregelix.JobRun.BasicGraphCleanTestSuite;
 
 public class BubbleMergeTestSuite extends BasicGraphCleanTestSuite {
 
     public static Test suite() throws Exception {
         String pattern = "BubbleMerge";
-        String testSet[] = { "TwoSimilarInFour"}; //"TwoSimilarInTwo"};
-//                "SimpleBubble", "LtoL", "LtoR", "RtoL", "RtoR", "FRandRF", "InvalidBubble",
-//                "BubbleWithTip", "SideBubble", "OverlapBubble", "SimpleRectangle", "MediumRectangle",
-//                "ComplexRectangle", "ThreeSquares", "Grid", "TwoSimilarInTwo", "TwoSimilarInThree", "TwoSimilarInFour",
-//                "ThreeSimilarInFour", "FourSimilarInFour"};
+        String testSet[] = { "TwoSimilarInTwo", "ThreeSimilarInThree", "TwoSimilarInThree", "TwoSimilarInFour" };
         init(pattern, testSet);
         BasicGraphCleanTestSuite testSuite = new BasicGraphCleanTestSuite();
         return makeTestSuite(testSuite);
--- genomix/genomix-pregelix/src/test/java/edu/uci/ics/genomix/pregelix/JobRun/test/BubbleAddTestSuite.java
+++ genomix/genomix-pregelix/src/test/java/edu/uci/ics/genomix/pregelix/JobRun/test/BubbleAddTestSuite.java
@@ -1,16 +1,15 @@
 package edu.uci.ics.genomix.pregelix.JobRun.test;
 
-import edu.uci.ics.genomix.pregelix.JobRun.BasicGraphCleanTestSuite;
 import junit.framework.Test;
+import edu.uci.ics.genomix.pregelix.JobRun.BasicGraphCleanTestSuite;
 
 public class BubbleAddTestSuite extends BasicGraphCleanTestSuite {
 
     public static Test suite() throws Exception {
         String pattern = "BubbleAdd";
-        String testSet[] = { "TwoSimilarInThree" };
+        String testSet[] = { "3", "TwoSimilarInTwo", "TwoSimilarInThree" };
         init(pattern, testSet);
         BasicGraphCleanTestSuite testSuite = new BasicGraphCleanTestSuite();
         return makeTestSuite(testSuite);
     }
 }
-  
