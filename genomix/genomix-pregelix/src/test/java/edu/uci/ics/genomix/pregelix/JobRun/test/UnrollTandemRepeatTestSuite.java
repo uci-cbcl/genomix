0f4617a6b5d7e628bf5a0ac6c8673e87c655d39e
Anbang Xu <anbangx@gmail.com>
2013-10-19 19:18:21 -0700

    Clean up UnrollTandenRepeat test cases and pass test

--- genomix/genomix-pregelix/src/test/java/edu/uci/ics/genomix/pregelix/JobRun/test/UnrollTandemRepeatTestSuite.java
+++ genomix/genomix-pregelix/src/test/java/edu/uci/ics/genomix/pregelix/JobRun/test/UnrollTandemRepeatTestSuite.java
@@ -1,13 +1,13 @@
 package edu.uci.ics.genomix.pregelix.JobRun.test;
 
-import edu.uci.ics.genomix.pregelix.JobRun.BasicGraphCleanTestSuite;
 import junit.framework.Test;
+import edu.uci.ics.genomix.pregelix.JobRun.BasicGraphCleanTestSuite;
 
 public class UnrollTandemRepeatTestSuite extends BasicGraphCleanTestSuite {
 
     public static Test suite() throws Exception {
         String pattern = "UnrollTandemRepeat";
-        String testSet[] = { "TandemRepeat", "BridgePathWithTandemRepeat" };
+        String testSet[] = { "SelfTandemRepeat" };
         init(pattern, testSet);
         BasicGraphCleanTestSuite testSuite = new BasicGraphCleanTestSuite();
         return makeTestSuite(testSuite);
