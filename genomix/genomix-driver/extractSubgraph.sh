2232754ecfeb3ddb1a96e55c80ed36c7f9a61b49
Anbang Xu <anbangx@gmail.com>
2013-10-24 17:13:37 -0700

    Add automatic run shell to extract subgraph

---
+++ genomix/genomix-driver/extractSubgraph.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#set -o pipefail
+#set -e
+#set -x
+
+if [ $# -ne 4 ]; then
+    echo "please provide 4 parameters: hdfsInput, localGraphOutput, startSeeds and numOfHops" 
+    echo "for example:   $0 /user/anbangx/test-extract-graph ~/graph/ CCCTCACTCTCCCCTAACGCCCCTGCCTCCCAGACCCACGACTCACAGGAGGAGC 2"
+    exit 1
+fi
+
+# cd genomix/genomix-pregelix/
+bin/genomix -kmerLength 55 -pipelineOrder EXTRACT_SUBGRAP -hdfsInput $1 -localGraphOutput $2 -hdfsWorkPath /user/anbangx/genomix-out-extract-subgraph -startSeedToExtractSubgraph $3 -numHopsToExtractSubgraph $4
+# cd ../..
