463f94a6075dd6045b961508696751d99ac02dbd
JavierJia <jianfeng.jia@gmail.com>
2013-10-25 18:01:45 -0700

    check ip address

--- genomix/genomix-driver/src/main/resources/scripts/getip.sh
+++ genomix/genomix-driver/src/main/resources/scripts/getip.sh
@@ -16,6 +16,8 @@ LINUX_OS='Linux'
         then
             IPADDR=`ifconfig lo | grep "inet " | awk '{print $2}' | cut -f 2 -d ':'`
         fi 
+        IS_VALID_IP=`echo $IPADDR | grep -E "([0-9]{1,3}\.{0,1}){4}"`
+        [ "$IS_VALID_IP" ] || IPADDR=`hostname -i`
 #    fi 
 #else
 #        IPADDR=`/sbin/ifconfig en1 | grep "inet " | awk '{print $2}' | cut -f 2 -d ':'`
