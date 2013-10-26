39b3368b6577e4307f4005c6471e25efe9b81643
JavierJia <jianfeng.jia@gmail.com>
2013-10-25 17:07:56 -0700

    checkout ipconfig

--- genomix/genomix-driver/src/main/resources/scripts/getip.sh
+++ genomix/genomix-driver/src/main/resources/scripts/getip.sh
@@ -4,25 +4,25 @@
 OS_NAME=`uname -a|awk '{print $1}'`
 LINUX_OS='Linux'
 
-if [ $OS_NAME = $LINUX_OS ];
-then
+#if [ $OS_NAME = $LINUX_OS ];
+#then
     #Get IP Address
     #Prefer Infiniband connection
 #    IPADDR=`/sbin/ifconfig ib0 2> /dev/null | grep "inet " | awk '{print $2}' | cut -f 2 -d ':'`
 #    if [ "$IPADDR" = "" ]
 #    then
-        IPADDR=`/sbin/ifconfig eth0 | grep "inet " | awk '{print $2}' | cut -f 2 -d ':'`
+        IPADDR=`ifconfig eth0 | grep "inet " | awk '{print $2}' | cut -f 2 -d ':'`
         if [ "$IPADDR" = "" ]
         then
-            IPADDR=`/sbin/ifconfig lo | grep "inet " | awk '{print $2}' | cut -f 2 -d ':'`
+            IPADDR=`ifconfig lo | grep "inet " | awk '{print $2}' | cut -f 2 -d ':'`
         fi 
 #    fi 
-else
-        IPADDR=`/sbin/ifconfig en1 | grep "inet " | awk '{print $2}' | cut -f 2 -d ':'`
-	if [ "$IPADDR" = "" ]
-        then
-                IPADDR=`/sbin/ifconfig lo0 | grep "inet " | awk '{print $2}' | cut -f 2 -d ':'`
-        fi
-
-fi
+#else
+#        IPADDR=`/sbin/ifconfig en1 | grep "inet " | awk '{print $2}' | cut -f 2 -d ':'`
+#	if [ "$IPADDR" = "" ]
+#        then
+#                IPADDR=`/sbin/ifconfig lo0 | grep "inet " | awk '{print $2}' | cut -f 2 -d ':'`
+#        fi
+#
+#fi
 echo $IPADDR
