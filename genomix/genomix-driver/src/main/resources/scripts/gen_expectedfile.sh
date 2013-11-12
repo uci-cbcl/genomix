efff5758b1b93d5b2bd6fca49660a55839b928f6
Anbang Xu <anbangx@gmail.com>
2013-11-12 14:19:40 -0800

    Add pipeline to expected generator

--- genomix/genomix-driver/src/main/resources/scripts/gen_expectedfile.sh
+++ genomix/genomix-driver/src/main/resources/scripts/gen_expectedfile.sh
@@ -8,22 +8,21 @@ cd "$genomix_home"
 #cd "../../../genomix-pregelix/src/test/resources/TestSet/expected/Reads/"
 #echo "`pwd -P`"
 
-input="../../../genomix-pregelix/src/test/resources/TestSet/Reads/"
-output="../../../genomix-pregelix/src/test/resources/TestSet/expected/"
+input="$genomix_home/../../../genomix-pregelix/src/test/resources/TestSet/Reads"
+output="$genomix_home/../../../genomix-pregelix/src/test/resources/TestSet/expected"
 
 rm -rf $output
 mkdir -p $output
 
 cd "$input"
-for pattern in MERGE; do
+for pattern in MERGE TIP_REMOVE LOW_COVERAGE BUBBLE BRIDGE SPLITREPEAT SCAFFOLD UNROLL_TANDEM CHECK_SYMMETRY PLOT_SUBGRAPH; do
         echo "Patern: $pattern"
 	pushd $pattern
 	for file in *; do
-		echo "$file"
-		$genomix_home/bin/genomix -kmerLength 3 -localInput $file -pipelineOrder BUILD,$pattern -localOutput $output/$pattern/$file -randomSeed 500 
+		echo "Running cmd = ($genomix_home/bin/genomix -kmerLength 3 -localInput $file -pipelineOrder BUILD,$pattern -localOutput $output/$pattern/$file -randomSeed 500 -runLocal)"
+		$genomix_home/bin/genomix -kmerLength 3 -localInput $file -pipelineOrder BUILD,$pattern -localOutput $output/$pattern/$file -randomSeed 500 -plotSubgraph_startSeed "AAT" -plotSubgraph_numHops 1
+		#sleep 30
 	done
 	popd
 done
-
-#cmd=(bin/genomix -kmerLength 3 bin/genomix -kmerLength 3 )
 
