62865b02f47f2c7e2ff4aa1d6ed3d3a602e6ff67
Anbang Xu <anbangx@gmail.com>
2013-11-13 16:00:03 -0800

    Clean up gabage file in auto scripts

--- genomix/genomix-driver/src/main/resources/scripts/gen_expectedfile.sh
+++ genomix/genomix-driver/src/main/resources/scripts/gen_expectedfile.sh
@@ -14,15 +14,46 @@ output="$genomix_home/../../../genomix-pregelix/src/test/resources/TestSet/expec
 rm -rf $output
 mkdir -p $output
 
+# automatically generate some patterns that can be directly generated from graph building
 cd "$input"
-for pattern in MERGE TIP_REMOVE LOW_COVERAGE BUBBLE BRIDGE SPLITREPEAT SCAFFOLD UNROLL_TANDEM CHECK_SYMMETRY PLOT_SUBGRAPH; do
+for pattern in MERGE LOW_COVERAGE SPLIT_REPEAT SCAFFOLD UNROLL_TANDEM CHECK_SYMMETRY PLOT_SUBGRAPH TIP_ADD BRIDGE_ADD; do
         echo "Patern: $pattern"
 	pushd $pattern
 	for file in *; do
-		echo "Running cmd = ($genomix_home/bin/genomix -kmerLength 3 -localInput $file -pipelineOrder BUILD,$pattern -localOutput $output/$pattern/$file -randomSeed 500 -runLocal)"
-		$genomix_home/bin/genomix -kmerLength 3 -localInput $file -pipelineOrder BUILD,$pattern -localOutput $output/$pattern/$file -randomSeed 500 -plotSubgraph_startSeed "AAT" -plotSubgraph_numHops 1
+		cmd="$genomix_home/bin/genomix -kmerLength 3 -localInput $file -pipelineOrder BUILD,$pattern -localOutput $output/$pattern/$file -randomSeed 500 -plotSubgraph_startSeed "AAT" -plotSubgraph_numHops 1 2>&1 | tee $output/$pattern/$file.log"
+		echo "Running cmd = ($cmd)"
+		eval "$cmd"
 		#sleep 30
 	done
+	# delete hdfs(here is current local directory) output folder
+	rm -rf genomix_out
 	popd
 done
 
+# TIP_REMOVE
+pattern="TIP_REMOVE"
+echo "Patern: $pattern"
+pushd $pattern
+for file in *; do
+	cmd="$genomix_home/bin/genomix -kmerLength 3 -localInput $file -pipelineOrder BUILD,TIP_ADD,$pattern -localOutput $output/$pattern/$file -randomSeed 500"
+	echo "Running cmd = ($cmd)"
+        eval "$cmd" 
+done
+rm -rf genomix_out
+popd
+
+# BRIDGE_REMOVE
+pattern="BRIDGE"
+echo "Patern: $pattern"
+pushd $pattern
+for file in *; do
+	cmd="$genomix_home/bin/genomix -kmerLength 3 -localInput $file -pipelineOrder BUILD,BRIDGE_ADD,$pattern -localOutput $output/$pattern/$file -randomSeed 500"	
+	echo "Running cmd = ($cmd)"
+        eval "$cmd"
+done
+rm -rf genomix_out
+popd
+
+# clean out output
+find $output/*/*/bin/ -name "*.crc" | xargs rm -f
+find $output/*/*/bin/ -name "_temporary" | xargs rm -rf
