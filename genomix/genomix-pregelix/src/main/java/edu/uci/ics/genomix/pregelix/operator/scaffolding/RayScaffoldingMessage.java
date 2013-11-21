d200f5a0b953675b34eb08718bb2985e156942af
Elmira <elmira.forouzmand@gmail.com>
2013-11-21 10:58:27 -0800

    ray scaffolding-adding details
    Conflicts:
    	genomix/genomix-pregelix/src/main/java/edu/uci/ics/genomix/pregelix/operator/scaffolding/RayScaffoldingMessage.java
    	genomix/genomix-pregelix/src/main/java/edu/uci/ics/genomix/pregelix/operator/scaffolding/RayVertex.java
    	genomix/genomix-pregelix/src/main/java/edu/uci/ics/genomix/pregelix/operator/scaffolding/ScaffoldingVertexValueWritable.java

---
+++ genomix/genomix-pregelix/src/main/java/edu/uci/ics/genomix/pregelix/operator/scaffolding/RayScaffoldingMessage.java
@@ -0,0 +1,172 @@
+package edu.uci.ics.genomix.pregelix.operator.scaffolding;
+
+import java.util.ArrayList;
+
+import edu.uci.ics.genomix.pregelix.io.VertexValueWritable;
+import edu.uci.ics.genomix.pregelix.io.message.MessageWritable;
+import edu.uci.ics.genomix.type.EDGETYPE;
+import edu.uci.ics.genomix.type.VKmer;
+import edu.uci.ics.genomix.type.VKmerList;
+
+
+public class RayScaffoldingMessage extends MessageWritable{
+	private ArrayList<VKmer> walk;
+	private boolean startFlag, computeFlag, previsitedFlag, neighborFlag,computeRulesFlag;
+	private boolean doneFlag, removeEdgesFlag, flipFlag;
+	private VKmer kmer, lastVertex;
+	private int walkSize;
+	private int index;
+	private int offset;
+	private int ruleA, ruleB, ruleC;
+	private EDGETYPE edgeType;
+	
+	public RayScaffoldingMessage() {
+        super();
+        walk = new ArrayList<VKmer>();
+    }
+	
+	public void reset() {
+        super.reset();
+        walk.clear();
+    }
+	
+	
+	public ArrayList<VKmer> getWalk() {
+		return walk;
+	}
+	
+	public void setWalk(ArrayList<VKmer> walk) {
+        this.walk = walk;
+    }
+	
+	public void setStartFlag() {
+        this.startFlag = true;
+    }
+	
+	public boolean getStartFlag(){
+		return this.startFlag;
+	}
+	
+	public void setKmer(VKmer internalKmer){
+		kmer = internalKmer;
+	}
+	
+	public VKmer getKmer(){
+		return this.kmer;
+	}
+	
+	public void setComputeFlag(){
+		this.computeFlag = true;
+	}
+	
+	public boolean getComputeFlag(){
+		return this.computeFlag;
+	}
+	
+	public void setWalkSize(int size){
+		this.walkSize = size;
+	}
+	
+	public void setIndex(int index){
+		this.index = index;
+	}
+	
+	public int getWalkSize(){
+		return this.walkSize;
+	}
+	
+	public int getIndex(){
+		return this.index ;
+	}
+	
+	public void setOffset(int offsetFromOneKmer){
+		this.offset = offsetFromOneKmer;
+	}
+	
+	public int getOffset(){
+		return this.offset;
+	}
+	
+	public void setRules(int ruleA, int ruleB, int ruleC){
+		this.ruleA = ruleA;
+		this.ruleC = ruleC;
+		this.ruleB = ruleB;
+	}
+	
+	public int getRuleA(){
+		return this.ruleA; 
+	}
+	
+	public int getRuleB(){
+		return this.ruleB; 
+	}
+	
+	public int getRuleC(){
+		return this.ruleC; 
+	}
+
+	public VKmer getLastVertex(){
+		return this.lastVertex;
+	}
+	
+	public void setLastVertex(VKmer lastVer){
+		this.lastVertex = lastVer;
+	}
+	
+	public void setPrevisitedFlag(){
+		this.previsitedFlag = true;
+	}
+	
+	public boolean getPrevisitedFlag(){
+		return this.previsitedFlag;
+	}
+	
+	public void setNeighborFlag() {
+        this.neighborFlag = true;
+    }
+	
+	public boolean getNeighborFlag(){
+		return this.neighborFlag;
+	}
+	
+	public void setComputeRulesFlag(){
+		this.computeRulesFlag = true;
+	}
+	
+	public boolean getComputeRulesFlag(){
+		return this.computeRulesFlag;
+	}
+	public void setDoneFlag(){
+		this.doneFlag = true;
+	}
+	
+	public boolean getDoneFlag(){
+		return this.doneFlag;
+	}
+	
+	public void setRemoveEdgesFlag(){
+		this.removeEdgesFlag = true;
+	}
+	
+	public boolean getRemoveEdgesFlag(){
+		return this.removeEdgesFlag;
+	}
+	
+	public void setEdgeType(EDGETYPE et){
+		this.edgeType = et;
+	}
+	
+	public EDGETYPE getEdgeType(){
+		return this.edgeType;
+	}
+	
+	public void setFlipFlag(){
+		this.flipFlag = true;
+	}
+	
+	public boolean getFlipFlag(){
+		return this.flipFlag;
+	}
+}
+
+
---
+++ genomix/genomix-pregelix/src/main/java/edu/uci/ics/genomix/pregelix/operator/scaffolding/RayVertex.java
@@ -0,0 +1,332 @@
+package edu.uci.ics.genomix.pregelix.operator.scaffolding;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import edu.uci.ics.genomix.config.GenomixJobConf;
+import edu.uci.ics.genomix.pregelix.io.VertexValueWritable;
+import edu.uci.ics.genomix.pregelix.io.message.MessageWritable;
+import edu.uci.ics.genomix.pregelix.operator.DeBruijnGraphCleanVertex;
+import edu.uci.ics.genomix.pregelix.operator.aggregator.StatisticsAggregator;
+import edu.uci.ics.genomix.pregelix.operator.scaffolding.BasicBFSTraverseVertex.SEARCH_TYPE;
+import edu.uci.ics.genomix.type.DIR;
+import edu.uci.ics.genomix.type.EDGETYPE;
+import edu.uci.ics.genomix.type.ReadHeadInfo;
+import edu.uci.ics.genomix.type.ReadHeadSet;
+import edu.uci.ics.genomix.type.VKmer;
+
+public class RayVertex extends DeBruijnGraphCleanVertex<ScaffoldingVertexValueWritable, RayScaffoldingMessage>{
+	public static int SCAFFOLDING_VERTEX_MIN_COVERAGE = -1;
+	//public int K = 21;
+	//private ArrayList<VKmer> neighbors;
+	//private boolean startPoint;
+	//private boolean preVisited;
+	// VKmer lastKmerInWalk;
+
+	public void initVertex(){	
+		///Complete this!
+		if (kmerSize == -1){
+            kmerSize = Integer.parseInt(getContext().getConfiguration().get(GenomixJobConf.KMER_LENGTH));
+		}
+		
+        if (SCAFFOLDING_VERTEX_MIN_COVERAGE < 0){
+            SCAFFOLDING_VERTEX_MIN_COVERAGE = Integer.parseInt(getContext().getConfiguration().get(
+                    GenomixJobConf.SCAFFOLDING_VERTEX_MIN_COVERAGE));
+        }
+        
+		if (outgoingMsg == null) {
+            outgoingMsg = new RayScaffoldingMessage();
+        } else {
+            outgoingMsg.reset();
+        }
+        
+    //=====================>>
+        //StatisticsAggregator.preGlobalCounters.clear();   
+        //counters.clear();
+        //getVertexValue().getCounters().clear();
+    //=====================>>	
+	    if (getVertexValue().walk == null) {
+	    	getVertexValue().walk = new ArrayList<VKmer>(); 
+	    } else{
+	    	getVertexValue().walk.clear();
+	    }
+	}
+
+	//We're finding the neighbors and send the walk to them
+	public void sendMsgToBranches() {
+		DIR direction;
+		if (getVertexValue().flipFalg){
+			direction = DIR.REVERSE;
+		} else {
+			direction = DIR.FORWARD;
+		}
+		//if there is just one neighbor?
+		for (EDGETYPE et : direction.edgeTypes()){
+			for (VKmer neighbor : getVertexValue().getEdgeMap(et).keySet()) {
+				sendMsgToNeighbor(neighbor, et);	
+				//I'm  not sure if we need to keep this edgetype
+			}
+		}
+	}
+	
+
+	
+	//tell neighbor about the walk
+	public void sendMsgToNeighbor(VKmer neighbor, EDGETYPE et){
+		outgoingMsg.setEdgeType(et);
+		outgoingMsg.setWalk(getVertexValue().walk);
+		outgoingMsg.setNeighborFlag();
+		//outgoingMsg.
+		//keep the decision point because we need to come back to it
+		outgoingMsg.setLastVertex(getVertexId());
+		sendMsg(neighbor ,outgoingMsg);
+		
+	}
+	
+
+	// for each Kmer in the walk and each BranchKmer finds the offset
+	public int offset(VKmer neighbor){
+		int offset = 0;
+		ReadHeadSet readIds;
+		if (getVertexValue().flipFalg){
+			readIds = getVertexValue().getFlippedReadIds();
+		} else {
+			readIds = getVertexValue().getUnflippedReadIds();
+		}
+		for (ReadHeadInfo read : readIds){
+			if (checkedDistance (read, neighbor)){
+				offset++;
+			}
+		}
+		return offset;
+	}	
+	
+	public boolean checkedDistance(ReadHeadInfo read, VKmer neighbor){
+		//if neighbor exist in read table
+		//How to work with a read?
+		read.getReadId();
+		read.getOffset();
+		int neighborPositionOnRead = getVertexValue().walkSize - getVertexValue().index + read.indexOf(getVertexId().toString()) + 1;
+		///I know!!
+		if(read.indexOf(neighbor.toString()) == neighborPositionOnread ){
+			return true;
+		}else{
+			return false;
+		}
+	}	
+	
+	
+		//compute the rules , add new sentence in each step	
+	public void rules_add(int offset){
+		//int l = walkSize + K - 1;
+		
+		getVertexValue().rules_a = getVertexValue().rules_a +  (getVertexValue().walkSize - getVertexValue().index) * offset;	
+		getVertexValue().rules_b = getVertexValue().rules_b + offset;
+		if ((offset < getVertexValue().rules_c) && (offset!= 0)){
+			getVertexValue().rules_c = offset;
+		}
+	}
+	
+	
+	public void prepareTheWinnerAndTheLoser(Iterator<RayScaffoldingMessage> msgIterator){
+		RayScaffoldingMessage incomingMsg;
+        while (msgIterator.hasNext()) {
+            incomingMsg = msgIterator.next();
+            if(incomingMsg.getStartFlag()){
+            	getVertexValue().walk = incomingMsg.getWalk();
+            	getVertexValue().flipFalg = incomingMsg.getFlipFlag();
+            	getVertexValue().startFlag = incomingMsg.getStartFlag();
+            }
+            else if (incomingMsg.getRemoveEdgesFlag()){
+            	getVertexValue().getEdgeMap(incomingMsg.getEdgeType().mirror()).remove(incomingMsg.getKmer());
+            }
+        }
+	}
+	
+	public void readWalkInfoAndSendOffsetFromOneKmer(Iterator<RayScaffoldingMessage> msgIterator){
+		RayScaffoldingMessage incomingMsg;
+		outgoingMsg.reset();
+		while(msgIterator.hasNext()){
+			incomingMsg = msgIterator.next();
+			if (incomingMsg.getComputeFlag()){
+				outgoingMsg.reset();
+				outgoingMsg.setComputeRulesFlag();
+				outgoingMsg.setEdgeType(incomingMsg.getEdgeType());
+				outgoingMsg.setOffset(offset(incomingMsg.getKmer()));
+				outgoingMsg.setIndex(incomingMsg.getIndex());
+				outgoingMsg.setWalkSize(incomingMsg.getWalkSize());			
+				sendMsg(incomingMsg.getKmer(), outgoingMsg);
+			}
+		}
+		
+	}
+	
+	public void sendMsgToWalkVertices(Iterator<RayScaffoldingMessage> msgIterator){
+		//Don't like it!
+		//And it's RC is not previsited
+		//If previsited it's not sending any message out.
+		if(!getVertexValue().previsitedFlag){
+			RayScaffoldingMessage incomingMsg;
+			while(msgIterator.hasNext()){
+				incomingMsg = msgIterator.next();	
+				if(incomingMsg.getNeighborFlag()){ 
+					outgoingMsg.reset();
+					//Sure?
+					outgoingMsg.setEdgeType(incomingMsg.getEdgeType());
+					outgoingMsg.setKmer(getVertexId()) ;
+					outgoingMsg.setComputeFlag();
+					outgoingMsg.setWalkSize(incomingMsg.getWalkSize());
+					for (VKmer vertexId : incomingMsg.getWalk()){
+						outgoingMsg.setIndex(incomingMsg.getWalk().indexOf(vertexId));
+						sendMsg(vertexId, outgoingMsg);
+					}
+				}
+			}
+			//Now you are a visited neighbor, we don't need you anymore
+			//Sure?
+			//getVertexValue().previsitedFlag = true;
+		}
+	}
+	
+	public void readOffsetInfoAndFindTheWholeOffset(Iterator<RayScaffoldingMessage> msgIterator){
+		RayScaffoldingMessage incomingMsg;
+		while(msgIterator.hasNext()){
+			incomingMsg = msgIterator.next();
+			if (incomingMsg.getComputeRulesFlag()){
+				getVertexValue().walkSize = incomingMsg.getWalkSize();
+				getVertexValue().index = incomingMsg.getIndex();
+				rules_add(incomingMsg.getOffset());
+				sendRuleValuestoLastVertex(incomingMsg.getEdgeType());
+			}
+		}	
+	}
+	
+	public void sendRuleValuestoLastVertex(EDGETYPE et){
+		outgoingMsg.reset();
+		outgoingMsg.setEdgeType(et);
+		outgoingMsg.setRules(getVertexValue().rules_a, getVertexValue().rules_b, getVertexValue().rules_c);
+		outgoingMsg.setKmer(getVertexId());
+		sendMsg(getVertexValue().lastKmer, outgoingMsg);
+	}
+	
+	public void chooseTheWinner(Iterator<RayScaffoldingMessage> msgIterator){
+		
+		//this is funny, do sth about it
+		RayScaffoldingMessage incomingMsg;
+		int ruleA = 0;
+		int ruleB = 0;
+		int ruleC = 0;
+		EDGETYPE edge = null;
+		VKmer winner = null; //humm?
+		if(msgIterator.hasNext()){
+			incomingMsg = msgIterator.next();
+			winner = incomingMsg.getKmer();
+			edge = incomingMsg.getEdgeType();
+			ruleA = incomingMsg.getRuleA();
+			ruleB = incomingMsg.getRuleB();
+			ruleC = incomingMsg.getRuleC();
+		}		  
+		while(msgIterator.hasNext()){
+			double m = getM();
+			incomingMsg = msgIterator.next();
+			if ((m * ruleA < incomingMsg.getRuleA()) && (m * ruleB <  incomingMsg.getRuleB()) && (m * ruleC < incomingMsg.getRuleC())){
+				sendRemoveEdgesMsgToLoser(winner, edge);
+				//Is this the right way?
+				getVertexValue().getEdgeMap(incomingMsg.getEdgeType()).remove(incomingMsg.getKmer());
+				winner = incomingMsg.getKmer();
+				ruleA = incomingMsg.getRuleA();
+				ruleB = incomingMsg.getRuleB();
+				ruleC = incomingMsg.getRuleC();	
+				edge = incomingMsg.getEdgeType();
+			} else {
+				sendRemoveEdgesMsgToLoser(incomingMsg.getKmer(), incomingMsg.getEdgeType());
+			}
+		}
+		// Do we really have a Winner?
+		if(ruleC == 0){
+			getVertexValue().doneFlag = true;
+			
+		} else {
+			outgoingMsg.reset();
+			if ((edge == EDGETYPE.RR) || (edge == EDGETYPE.FR)){
+				outgoingMsg.setFlipFlag();
+			}
+			outgoingMsg.setWalk(getVertexValue().walk);
+			outgoingMsg.setStartFlag();
+			sendMsg(winner, outgoingMsg);
+			//return winner;
+		}
+		
+	}
+
+	public void sendRemoveEdgesMsgToLoser(VKmer loser, EDGETYPE et){
+		outgoingMsg.reset();
+		outgoingMsg.setEdgeType(et);
+		outgoingMsg.setKmer(getVertexId());
+		outgoingMsg.setRemoveEdgesFlag();
+		sendMsg(loser,outgoingMsg);
+	}
+	//Is it the only way?
+	public void removeLoserEdges(Iterator<RayScaffoldingMessage> msgIterator){
+		RayScaffoldingMessage incomingMsg;
+        while (msgIterator.hasNext()) {
+            incomingMsg = msgIterator.next();
+            if (incomingMsg.getRemoveEdgesFlag()){
+            	getVertexValue().getEdgeMap(incomingMsg.getEdgeType().mirror()).remove(incomingMsg.getKmer());
+            }	//getVertexValue().processDelete(neighborToDeleteEdgetype, keyToDelete);
+        }
+	}
+		
+	public double getM(){
+		double m = 0;
+		float coverage = getVertexValue().getAverageCoverage();
+		if ((coverage>=2) && (coverage <= 19)){
+			m = 3;
+		} else if ((coverage>=20) && (coverage <= 24)){
+			m = 2;
+		} else if ((coverage>=25)){
+			m = 1.3;
+		}
+		return m;
+	}
+	
+	public void scaffold(Iterator<RayScaffoldingMessage> msgIterator){
+		
+		if (getVertexValue().getAverageCoverage() > SCAFFOLDING_VERTEX_MIN_COVERAGE){
+			initVertex();
+		} else {
+			voteToHalt();
+		}
+		if (!getVertexValue().doneFlag){
+			if (getSuperstep() == 1) {
+				sendMsgToBranches(DIR.FORWARD, msgIterator);
+	        } else if (getSuperstep() == 2){
+	        	//Next Step you are that neighbor
+	    		sendMsgToWalkVertices(msgIterator);
+	        } else if (getSuperstep() == 3){
+	        	//Now you are one of the vertices in the walk
+	    		readWalkInfoAndSendOffsetFromOneKmer(msgIterator);		
+	        } else if (getSuperstep() == 4){
+	        	//Now you are the neighbor again
+	    		readOffsetInfoAndFindTheWholeOffset(msgIterator);
+	        } else if (getSuperstep() == 5){
+	        	//Now you are the last Vertex Again
+	    		//You have the values to compare
+	    		chooseTheWinner(msgIterator);
+	        } else if (getSuperstep() == 5){
+	        	//Again you are the neighbor and you need to remove those extra edges
+	    		removeLoserEdges(msgIterator);	
+	        }
+		}
+			
+			
+	}
+	
+
+	@Override
+	public void compute(Iterator<RayScaffoldingMessage> msgIterator) throws Exception {
+		// TODO Auto-generated method stub
+	}
+	
+
+}
