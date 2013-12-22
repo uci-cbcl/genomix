25781260791ca34d757cc25acb0c3e9b2f478105
Nan Zhang <zhangnan2920214@gmail.com>
2013-12-22 09:28:26 -0800

    start readHeadInfoRandomTest

---
+++ genomix/genomix-data/src/test/java/edu/uci/ics/genomix/data/types/ReadHeadInfoRandomTest.java
@@ -0,0 +1,14 @@
+package edu.uci.ics.genomix.data.types;
+
+import java.util.Random;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import edu.uci.ics.genomix.data.types.ReadHeadInfo;
+import edu.uci.ics.genomix.data.utils.Marshal;
+
+public class ReadHeadInfoRandomTest {
+    
+}
