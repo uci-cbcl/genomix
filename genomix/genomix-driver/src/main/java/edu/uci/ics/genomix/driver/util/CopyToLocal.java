b0b70d7a44df20879d69bd6a147e49cd7d5895e1
JavierJia <jianfeng.jia@gmail.com>
2014-01-02 12:14:09 -0800

    update configuration

--- genomix/genomix-driver/src/main/java/edu/uci/ics/genomix/driver/util/CopyToLocal.java
+++ genomix/genomix-driver/src/main/java/edu/uci/ics/genomix/driver/util/CopyToLocal.java
@@ -3,6 +3,7 @@ package edu.uci.ics.genomix.driver.util;
 import java.io.IOException;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapred.JobConf;
 import org.kohsuke.args4j.CmdLineException;
 
@@ -12,6 +13,10 @@ import edu.uci.ics.genomix.data.cluster.GenomixClusterManager;
 public class CopyToLocal {
     public static void main(String[] args) throws CmdLineException, IOException {
         Configuration conf = new Configuration();
+        String PATH_TO_HADOOP_CONF = System.getenv("HADOOP_HOME") + "/conf";
+        conf.addResource(new Path(PATH_TO_HADOOP_CONF + "/core-site.xml"));
+        conf.addResource(new Path(PATH_TO_HADOOP_CONF + "/mapred-site.xml"));
+        conf.addResource(new Path(PATH_TO_HADOOP_CONF + "/hdfs-site.xml"));
         JobConf job = new JobConf(conf);
         if (args.length < 2) {
             System.err.println("Usage: copytolocal <hdfsSrcDir> <localDestDir>");
